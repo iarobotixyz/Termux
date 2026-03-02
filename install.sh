49fdf6295768276f5e104bdfe8089c365a4acfc8
iarobotixyz <62524097+iarobotixyz@users.noreply.github.com>
2026-03-02 01:30:40 -0600

    Add installation script for RoboticaXYZ Termux Pro
    This script installs RoboticaXYZ Termux Pro 1.0 and necessary packages.

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+clear
+echo "Instalando RoboticaXYZ Termux Pro 1.0..."
+
+pkg update -y
+pkg install git curl figlet neofetch -y
+
+#cd $HOME
+#git clone https://github.com/roboticaxyz/termux-pro.git
+echo "Descargando roboticaXYZ....."
+
+#cd termux-pro
+#chmod +x *.sh
+#chmod +x modules/*.sh
+
+echo "Instalaci贸n completada."
+
+echo "Ejecuta con: bash roboticaxyz.sh"
